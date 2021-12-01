888c40cbf7f1866bdb196d5d7824c2c5f9123f09
jheer <jonas.heer@soprasteria.com>
2021-12-01 23:21:06 +0100

    Day 1 part 2

--- day01-sonar-sweep/Program.fsx
+++ day01-sonar-sweep/Program.fsx
@@ -1,8 +1,20 @@
 open System.IO
 
-let result =
+let depths =
     File.ReadLines("day01-sonar-sweep/input.txt")
     |> Seq.map int
+
+let result1 =
+    depths
+    |> Seq.pairwise
+    |> Seq.map (fun (a, b) -> b > a)
+    |> Seq.filter id
+    |> Seq.length
+
+let result2 =
+    depths
+    |> Seq.windowed 3
+    |> Seq.map (fun window -> Seq.reduce (+) window)
     |> Seq.pairwise
     |> Seq.map (fun (a, b) -> b > a)
     |> Seq.filter id
