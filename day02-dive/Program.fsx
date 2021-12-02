7aae08a8169cb0d7bf5fa64ea83f5bddf4537664
Jonas Gunneng Heer <jonas.g.heer@gmail.com>
2021-12-02 20:39:12 +0100

    Updated day 2. Inspired by JanDotNet

--- day02-dive/Program.fsx
+++ day02-dive/Program.fsx
@@ -16,31 +16,24 @@ let commands =
     File.ReadLines("day02-dive/input.txt")
     |> Seq.map makeCommand
 
-// Todo: find a solution without mutible vars
+let followCmd1 (d, h) (cmd) =
+    match cmd with
+    | Forward x -> (d, h + x)
+    | Down x -> (d + x, h)
+    | Up x -> (d - x, h)
+
+let followCmd2 (d, h, a) (cmd) =
+    match cmd with
+    | Forward x -> (d + a * x, h + x, a)
+    | Down x -> (d, h, a + x)
+    | Up x -> (d, h, a - x)
 
 let result1 =
-    let mutable depth = 0
-    let mutable horizontal = 0
-
-    for c in commands do
-        match c with
-        | Forward x -> horizontal <- horizontal + x
-        | Down x -> depth <- depth + x
-        | Up x -> depth <- depth - x
-
-    depth * horizontal
+    commands
+    |> Seq.fold followCmd1 (0, 0)
+    |> (fun (d, h) -> d * h)
 
 let result2 =
-    let mutable depth = 0
-    let mutable horizontal = 0
-    let mutable aim = 0
-
-    for c in commands do
-        match c with
-        | Forward x ->
-            horizontal <- horizontal + x
-            depth <- depth + aim * x
-        | Down x -> aim <- aim + x
-        | Up x -> aim <- aim - x
-
-    depth * horizontal
+    commands
+    |> Seq.fold followCmd2 (0, 0, 0)
+    |> (fun (d, h, _) -> d * h)
