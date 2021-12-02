5b9aaab3eaae57e2cfbbdd1416cb6997fe3ba663
Jonas Gunneng Heer <jonas.g.heer@gmail.com>
2021-12-02 19:58:49 +0100

    Day 2 part 2 (with mutables)

--- day02-dive/Program.fsx
+++ day02-dive/Program.fsx
@@ -1,15 +1,46 @@
 open System.IO
 
+type Command =
+    | Forward of int
+    | Down of int
+    | Up of int
+
+let makeCommand (cmdString: string) =
+    match cmdString.Split(' ') with
+    | [| "forward"; x |] -> Forward(x |> int)
+    | [| "down"; x |] -> Down(x |> int)
+    | [| "up"; x |] -> Up(x |> int)
+    | _ -> failwith "invalid command string"
+
+let commands =
+    File.ReadLines("day02-dive/input.txt")
+    |> Seq.map makeCommand
+
 // Todo: find a solution without mutible vars
+
 let result1 =
     let mutable depth = 0
     let mutable horizontal = 0
 
-    for line in File.ReadLines("day02-dive/input.txt") do
-        match line.Split(' ') with
-        | [| "forward"; x |] -> horizontal <- horizontal + (x |> int)
-        | [| "down"; x |] -> depth <- depth + (x |> int)
-        | [| "up"; x |] -> depth <- depth - (x |> int)
-        | _ -> failwith "invalid command string"
+    for c in commands do
+        match c with
+        | Forward x -> horizontal <- horizontal + x
+        | Down x -> depth <- depth + x
+        | Up x -> depth <- depth - x
+
+    depth * horizontal
+
+let result2 =
+    let mutable depth = 0
+    let mutable horizontal = 0
+    let mutable aim = 0
+
+    for c in commands do
+        match c with
+        | Forward x ->
+            horizontal <- horizontal + x
+            depth <- depth + aim * x
+        | Down x -> aim <- aim + x
+        | Up x -> aim <- aim - x
 
     depth * horizontal
