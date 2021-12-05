3d37e63b7cdeaf8f2d3f3d091eca2711ebef8815
Jonas Gunneng Heer <jonas.g.heer@gmail.com>
2021-12-05 17:52:34 +0100

    Day 3 part 2 - oh my god recursion

--- day03-binary-diagnostic/Program.fsx
+++ day03-binary-diagnostic/Program.fsx
@@ -4,19 +4,62 @@ let lines =
     File.ReadLines("day03-binary-diagnostic/input.txt")
 
 let reports =
-    lines |> Seq.map (fun n -> n.ToCharArray())
+    lines
+    |> Seq.map (fun n -> n.ToCharArray())
+    |> Seq.toList
+
+let charArrayToInt (chars: char []) =
+    System.Convert.ToInt32(new System.String(chars), 2)
 
 let findMetric reports cmpFunc =
     reports
     |> Seq.transpose
     |> Seq.map (Seq.countBy id)
-    |> Seq.map (cmpFunc (fun (char, count) -> count))
+    |> Seq.map (cmpFunc (fun (_, count) -> count))
     |> Seq.map fst
     |> Seq.toArray
-    |> (fun chars -> System.Convert.ToInt32(new System.String(chars), 2))
+    |> charArrayToInt
 
 let gammaRate reports = findMetric reports Seq.maxBy
 
 let eposilonRate reports = findMetric reports Seq.minBy
 
 let result1 = gammaRate reports * eposilonRate reports
+
+let mostCommonAtIndex reports index =
+    reports
+    |> Seq.transpose
+    |> Seq.item index
+    |> Seq.countBy id
+    |> Seq.sortByDescending id // to make sure '1' is returned if count is equal
+    |> Seq.maxBy (fun (_, count) -> count)
+    |> fst
+
+let leastCommonAtIndex reports index =
+    reports
+    |> Seq.transpose
+    |> Seq.item index
+    |> Seq.countBy id
+    |> Seq.sortBy id // to make sure '1' is returned if count is equal
+    |> Seq.minBy (fun (_, count) -> count)
+    |> fst
+
+let rec filterDownReports remainingReports index x cmpFunc =
+    match List.filter (fun r -> (Seq.item index r) = x) remainingReports with
+    | [ report ] -> [ report ]
+    | reports ->
+        let newIndex = index + 1
+        filterDownReports reports newIndex (cmpFunc reports newIndex) cmpFunc
+
+let oxygenGeneratorRating =
+    filterDownReports reports 0 (mostCommonAtIndex reports 0) mostCommonAtIndex
+    |> List.head
+    |> charArrayToInt
+
+let co2ScrubberRating =
+    filterDownReports reports 0 (leastCommonAtIndex reports 0) leastCommonAtIndex
+    |> List.head
+    |> charArrayToInt
+
+let result2 =
+    oxygenGeneratorRating * co2ScrubberRating
